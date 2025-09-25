16acdff98a274c35fde9331487af2647c604c8cd
edsonplz <edson.silva@dcx.ufpb.br>
2025-09-25 09:33:55 -0300

    Commit inicial, Spring Inicializer

---
+++ src/test/java/com/barberpro/dsc/DscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barberpro.dsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
